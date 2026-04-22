61d42f361e5651f5d0454ce8193341b5cb4b91a6
bbw1081 <181002857+bbw1081@users.noreply.github.com>
2026-04-22 13:12:43 -0400

    Preset dump

---
+++ example_presets/wavefold_subtle.h
@@ -0,0 +1,8 @@
+#pragma once
+
+const char preset[] = R"({
+    "name": "subtle wavefolder",
+    "effects": ["wavefolder"],
+    "params": [[0.3, 0.5]],
+    "description": "mild wave folding distortion; gentle harmonic enhancement with subtle grit"
+})";
